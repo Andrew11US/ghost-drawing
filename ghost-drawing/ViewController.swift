e55a8bfa5d39f24ad590b036bb340e9eb5fb97cb
Andrii Halabuda <andrii.halabuda@gmail.com>
2022-07-20 16:25:46 -0400

    Initial Commit

---
+++ ghost-drawing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ghost-drawing
+//
+//  Created by Andrew on 2022-07-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
