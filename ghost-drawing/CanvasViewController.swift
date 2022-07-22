e1ec2b52323300db12806464a25ecbc364993b1c
Andrii Halabuda <andrii.halabuda@gmail.com>
2022-07-21 20:14:24 -0400

    Project structure setup

--- ghost-drawing/CanvasViewController.swift
+++ ghost-drawing/CanvasViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CanvasViewController.swift
 //  ghost-drawing
 //
 //  Created by Andrew on 2022-07-20.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CanvasViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
