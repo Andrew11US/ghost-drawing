eac1ae4c2f088b3c8c7618dc607cb2c20716fd38
Andrii Halabuda <andrii.halabuda@gmail.com>
2022-07-21 21:27:10 -0400

    Added CanvasView and implemented basic drawing functionality

--- ghost-drawing/CanvasViewController.swift
+++ ghost-drawing/CanvasViewController.swift
@@ -8,10 +8,36 @@
 import UIKit
 
 class CanvasViewController: UIViewController {
-
+    private(set) lazy var canvasView: CanvasView = makeCanvasView()
+    
+    // MARK: - View lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(canvasView)
+        view.bringSubviewToFront(canvasView)
+        setupConstraints()
+    }
+    
+    // MARK: - Methods
+    private func setupConstraints() {
+        canvasView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            canvasView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            canvasView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            canvasView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            canvasView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+}
+
+// MARK: - CanvasViewController extensions
+private extension CanvasViewController {
+    func makeCanvasView() -> CanvasView {
+        let canvasView = CanvasView()
+        canvasView.backgroundColor = .white
+        return canvasView
     }
 }
 
---
+++ ghost-drawing/Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  ghost-drawing
+//
+//  Created by Andrew on 2022-07-21.
+//
+
+import UIKit
+
+struct Line {
+    var points: [CGPoint]
+    let strokeColor: UIColor
+    let strokeWidth: CGFloat
+}
